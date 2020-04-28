1e035159da9d5891f541c64c484751fbb9fc4623
blyat <samuel@iptoki.com>
2020-04-28 14:18:52 -0400

    change http status code
    Signed-off-by: blyat <samuel@iptoki.com>

--- src/schemas/emails.js
+++ src/schemas/emails.js
@@ -60,7 +60,7 @@ module.exports = {
 		error("email_invalid_activation_token", 403, "The supplied email activation token is invalid or has expired"),
 
 	DeleteNotAllowedError:
-		error("delete_email_error", 405, "Cannot delete the supplied email because users should have at least one email address linked to their account"),
+		error("delete_email_error", 403, "Cannot delete the supplied email because users should have at least one email address linked to their account"),
 
 	EmailAlreadyActivatedError:
 		error("user_email_already_active", 412, "This email is already active and cannot be activated again"),
