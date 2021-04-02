07371aace54e8f7cb833a6364db5ff5edcb6fd80
blyatsama <samuel@iptoki.com>
2021-04-02 14:25:14 -0400

    saving work
    Signed-off-by: blyatsama <samuel@iptoki.com>

---
+++ src/routes/workpieces/editorSplits.js
@@ -0,0 +1,10 @@
+const Errors = require('../../errors')
+const JWTAuth = require('../../service/JWTAuth')
+
+/************************ Routes ************************/
+
+async function routes(fastify, options) {}
+
+/************************ Handlers ************************/
+
+module.exports = routes
